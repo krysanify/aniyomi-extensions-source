9e4e7c20ab665fb1e045339465a6cf2d1769d54f
Your Name <you@example.com>
2025-01-07 20:23:43 +0800

    Remove log

--- lib/megacloud-extractor/src/main/java/eu/kanade/tachiyomi/lib/megacloudextractor/WebViewResolver.kt
+++ lib/megacloud-extractor/src/main/java/eu/kanade/tachiyomi/lib/megacloudextractor/WebViewResolver.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.webkit.ConsoleMessage
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
@@ -21,14 +20,12 @@ class WebViewResolver(private val globalHeaders: Headers) {
     private val context: Application by injectLazy()
     private val handler by lazy { Handler(Looper.getMainLooper()) }
     private val json: Json by injectLazy()
-    private val tag by lazy { javaClass.simpleName }
 
     class JsInterface(private val latch: CountDownLatch) {
         var result: String? = null
 
         @JavascriptInterface
         fun setResponse(response: String) {
-            Log.d("WebViewResolver", "script result: $response")
             result = response
             latch.countDown()
         }
@@ -60,31 +57,19 @@ class WebViewResolver(private val globalHeaders: Headers) {
 
             webview.webViewClient = object : WebViewClient() {
                 override fun onPageFinished(view: WebView?, url: String?) {
-                    Log.d(tag, "onPageFinished $url")
                     super.onPageFinished(view, url)
 
-                    Log.d(tag, "injecting scripts")
                     view?.evaluateJavascript(getJsContent("/assets/crypto-js.js")) {}
                     view?.evaluateJavascript(getJsContent("/assets/megacloud.decodedpng.js")) {}
                     view?.evaluateJavascript(getJsContent("/assets/megacloud.getsrcs.js")) {}
-
-                    Log.d(tag, "running script")
                     view?.evaluateJavascript(
-                        "getSources(\"${xrax}\")" +
-                            ".then( s => jsinterface.setResponse( JSON.stringify(s) ) )",
+                        "getSources(\"${xrax}\").then( s => jsinterface.setResponse( JSON.stringify(s) ) )",
                     ) {}
                 }
             }
 
             webview.webChromeClient = object : WebChromeClient() {
                 override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                    Log.d(
-                        tag,
-                        "Chrome: [${consoleMessage?.messageLevel()}]" +
-                            "${consoleMessage?.message()}" +
-                            " at ${consoleMessage?.lineNumber()}" +
-                            " in ${consoleMessage?.sourceId()}",
-                    )
                     return super.onConsoleMessage(consoleMessage)
                 }
             }
